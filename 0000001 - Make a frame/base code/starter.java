3a1ed4432535b00e094f15280699667ad8928c5a
gregneat <39938260+gregneat@users.noreply.github.com>
2018-08-15 22:22:57 -0700

    intall base code for 1st 2 labs, add folder 0

---
+++ 0000001 - Make a frame/base code/starter.java
@@ -0,0 +1,18 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import javax.swing.*;
+import pkg.*;
+
+
+public class starter 
+{
+	public static void main(String[] args) 
+	{
+		// put your code here
+		
+	}
+}
+
+
